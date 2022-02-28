e636cbc7be701e23722276acf19acc333a14dffa
Man Ping Chen <manpingchen@gmail.com>
2022-02-28 13:57:21 +0000

    Add Redirect and Nested Route

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,6 +1,16 @@
+import { Route, useParams } from "react-router-dom";
+import Comments from "../components/comments/Comments";
+
 const QuoteDetail = () => {
-    return <h1>Quote</h1>;
-  };
-  
-  export default QuoteDetail;
-  
+  const params = useParams();
+  return (
+    <section>
+      <h1>Quote for {params.quoteId}</h1>
+      <Route path="/quotes/:quoteId/comments">
+        <Comments />
+      </Route>
+    </section>
+  );
+};
+
+export default QuoteDetail;
