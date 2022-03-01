d15e7bc608877edeb2d5c30fbc946c45ea30e6e6
Man Ping Chen <manpingchen@gmail.com>
2022-03-01 16:19:59 +0000

    Add router prompt and button disabled state

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,12 +1,21 @@
-import { useRef } from 'react';
+import { Fragment, useRef, useState } from "react";
 
-import Card from '../UI/Card';
-import LoadingSpinner from '../UI/LoadingSpinner';
-import classes from './QuoteForm.module.css';
+import { Prompt } from "react-router-dom";
+
+import Card from "../UI/Card";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import classes from "./QuoteForm.module.css";
 
 const QuoteForm = (props) => {
   const authorInputRef = useRef();
   const textInputRef = useRef();
+
+  const [isEntering, setIsEntering] = useState(false);
+  const [isFormReady, setIsFormReady] = useState(false);
+
+  const formChangeHandler = () => {
+    setIsFormReady(Boolean(authorInputRef.current.value && textInputRef.current.value));
+  };
 
   function submitFormHandler(event) {
     event.preventDefault();
@@ -19,28 +28,50 @@ const QuoteForm = (props) => {
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
 
-  return (
-    <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
-        {props.isLoading && (
-          <div className={classes.loading}>
-            <LoadingSpinner />
-          </div>
-        )}
+  const finishEnteringHandler = () => {
+    setIsEntering(false);
+    console.log("Click");
+  };
 
-        <div className={classes.control}>
-          <label htmlFor='author'>Author</label>
-          <input type='text' id='author' ref={authorInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='text'>Text</label>
-          <textarea id='text' rows='5' ref={textInputRef}></textarea>
-        </div>
-        <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
-        </div>
-      </form>
-    </Card>
+  const formFocusedHandler = () => {
+    setIsEntering(true);
+  };
+
+  return (
+    <Fragment>
+      <Prompt
+        when={isEntering}
+        message={(location) => "Are you sure you want to leave? The entered messages will be lost."}
+      />
+      <Card>
+        <form
+          onFocus={formFocusedHandler}
+          onChange={formChangeHandler}
+          className={classes.form}
+          onSubmit={submitFormHandler}
+        >
+          {props.isLoading && (
+            <div className={classes.loading}>
+              <LoadingSpinner />
+            </div>
+          )}
+
+          <div className={classes.control}>
+            <label htmlFor="author">Author</label>
+            <input type="text" id="author" ref={authorInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="text">Text</label>
+            <textarea id="text" rows="5" ref={textInputRef}></textarea>
+          </div>
+          <div className={classes.actions}>
+            <button className="btn" disabled={!isFormReady} onClick={finishEnteringHandler}>
+              Add Quote
+            </button>
+          </div>
+        </form>
+      </Card>
+    </Fragment>
   );
 };
 
--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,5 +1,15 @@
+import { useHistory } from "react-router-dom";
+import QuoteForm from "../components/quotes/QuoteForm";
+
 const NewQuote = () => {
-  return <h1>New Quote</h1>;
+  const history = useHistory();
+
+  const addQuoteHandler = (newQuote) => {
+    console.log(newQuote);
+
+    history.push("/quotes"); // history.push enable users to go back to the page by clicking back button on browser
+  };
+  return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
 
 export default NewQuote;
