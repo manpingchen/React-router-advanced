284018494a2f6f0d507f4536ff8c1a85892645b4
Man Ping Chen <manpingchen@gmail.com>
2022-03-01 15:55:37 +0000

    Render quote detail page

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,11 +1,25 @@
 import { Route, useParams } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning is fun" },
+  { id: "q2", author: "Marco", text: "Learning is not fun" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found</p>;
+  }
+
   return (
     <section>
       <h1>Quote for {params.quoteId}</h1>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/:${params.quoteId}/comments`}>
         <Comments />
       </Route>
