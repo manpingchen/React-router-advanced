8a03f9b427f1b1bed79bbac98f24b426de617414
Man Ping Chen <manpingchen@gmail.com>
2022-03-03 12:23:02 +0000

    Optimize code by implementing React Lazy Loading

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/layout/Layout";
-import AllQuotes from "./pages/AllQuotes";
-import NewQuote from "./pages/NewQuote";
-import NotFound from "./pages/NotFound";
-import QuoteDetail from "./pages/QuoteDetail";
-import Comments from "./components/comments/Comments";
+import { lazy, Suspense } from "react";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const NewQuote = lazy(() => import("./pages/NewQuote"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+const QuoteDetail = lazy(() => import("./pages/QuoteDetail"));
+const AllQuotes = lazy(() => import("./pages/AllQuotes"));
+const Comments = lazy(() => import("./components/comments/Comments"));
 
 function App() {
   return (
     <Layout>
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/quotes" />} />
-        <Route path="quotes">
-          <Route index element={<AllQuotes />} />
-          <Route path=":quoteId/*" element={<QuoteDetail />}>
-            <Route path="comments" element={<Comments />} />
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/quotes" />} />
+          <Route path="quotes">
+            <Route index element={<AllQuotes />} />
+            <Route path=":quoteId/*" element={<QuoteDetail />}>
+              <Route path="comments" element={<Comments />} />
+            </Route>
           </Route>
-        </Route>
-        <Route path="new-quote" element={<NewQuote />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+          <Route path="new-quote" element={<NewQuote />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </Layout>
   );
 }
